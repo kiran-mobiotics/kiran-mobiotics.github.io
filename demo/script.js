31719e4c01c08e36f00a55e09f308213c1cdc444
Kiran Pankan <pankan@Magic-Pot-Pro.local>
2017-08-29 18:38:56 +0530

    nr

--- demo/script.js
+++ demo/script.js
@@ -1,17 +1,17 @@
 function mark() {
 
     $('.fhd').watermark({
-        text: $('#version').val()+"&nbsp;"+$('#build').val(),
+        text: $('#version').val()+"\r\n"+$('#build').val(),
         textWidth: 100,
         textSize: 26
     });
     $('.hd').watermark({
-        text: $('#version').val()+"&nbsp;"+$('#build').val(),
+        text: $('#version').val()+"\r\n"+$('#build').val(),
         textWidth: 100,
         textSize: 15
     });
     $('.sd').watermark({
-        text: $('#version').val()+"&nbsp;"+$('#build').val(),
+        text: $('#version').val()+"\r\n"+$('#build').val(),
         textWidth: 100,
         textSize: 13
     });
