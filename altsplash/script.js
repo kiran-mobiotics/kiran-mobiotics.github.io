02426cc3a68cd55148b0c4a25cb0a8c5d335ef13
Kiran Pankan <pankan@Magic-Pot-Pro.local>
2017-08-29 18:50:21 +0530

    name

--- altsplash/script.js
+++ altsplash/script.js
@@ -3,17 +3,20 @@ function mark() {
     $('.fhd').watermark({
         text: $('#version').val()+" "+$('#build').val(),
         textWidth: 300,
-        textSize: 26
+        textSize: 26,
+        margin: 150
     });
     $('.hd').watermark({
         text: $('#version').val()+" "+$('#build').val(),
         textWidth: 100,
-        textSize: 15
+        textSize: 15,
+        margin: 100
     });
     $('.sd').watermark({
         text: $('#version').val()+" "+$('#build').val(),
         textWidth: 80,
-        textSize: 13
+        textSize: 13,
+        margin: 75
     });
 
 }
