f2bab5dc3e3e2d4d80149b589ee621f238e2b182
Kiran Pankan <pankan@Magic-Pot-Pro.local>
2017-08-29 18:40:33 +0530

    rem nr

--- demo/script.js
+++ demo/script.js
@@ -1,18 +1,18 @@
 function mark() {
 
     $('.fhd').watermark({
-        text: $('#version').val()+"\r\n"+$('#build').val(),
-        textWidth: 100,
+        text: $('#version').val()+" "+$('#build').val(),
+        textWidth: 300,
         textSize: 26
     });
     $('.hd').watermark({
-        text: $('#version').val()+"\r\n"+$('#build').val(),
+        text: $('#version').val()+" "+$('#build').val(),
         textWidth: 100,
         textSize: 15
     });
     $('.sd').watermark({
-        text: $('#version').val()+"\r\n"+$('#build').val(),
-        textWidth: 100,
+        text: $('#version').val()+" "+$('#build').val(),
+        textWidth: 80,
         textSize: 13
     });
 
