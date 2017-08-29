e980b396374cabc4ae542214ab69f2a6d5a6e502
Kiran Pankan <pankan@Magic-Pot-Pro.local>
2017-08-29 19:19:45 +0530

    css

--- altsplash/script.js
+++ altsplash/script.js
@@ -3,19 +3,19 @@ function mark() {
     $('.fhd').watermark({
         text: $('#version').val()+"."+$('#build').val(),
         textWidth: 300,
-        textSize: 26,
+        textSize: $('#fnt-siz').val(),
         margin: 150
     });
     $('.hd').watermark({
         text: $('#version').val()+"."+$('#build').val(),
         textWidth: 100,
-        textSize: 15,
+        textSize: $('#fnt-siz').val()*2/3,
         margin: 100
     });
     $('.sd').watermark({
         text: $('#version').val()+"."+$('#build').val(),
         textWidth: 80,
-        textSize: 13,
+        textSize: $('#fnt-siz').val()/2 ,
         margin: 75
     });
 
