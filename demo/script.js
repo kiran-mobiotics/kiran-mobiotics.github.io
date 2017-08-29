836f6f46763bd746dfc5bf0b3aa3a2a77b83c8b6
Kiran Pankan <pankan@Magic-Pot-Pro.local>
2017-08-29 17:51:08 +0530

    watermark

--- demo/script.js
+++ demo/script.js
@@ -1,14 +1,4 @@
 function demo() {
-    var inputImages = ['https://i.imgur.com/AAPx3rB.jpg', 'https://i.imgur.com/39dfdPw.jpg'];
-    $.each(inputImages, function (i, v) {
-        $('<img>', {
-            src: v
-        }).watermark({
-            done: function (imgURL) {
-                $('#content').append('<p><img src="' + imgURL + '"></p>');
-            }
-        });
-    });
 
     $('.watermark').watermark({
         text: 'GOOGLE.COM',
